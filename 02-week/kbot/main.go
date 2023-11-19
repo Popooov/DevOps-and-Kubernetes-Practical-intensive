8c13d7cb53033fa572d67e2ef9094d6435c36f5b
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2023-11-19 15:49:33 +0000

    kbot

---
+++ 02-week/kbot/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/Popooov/DevOps-and-Kubernetes-Practical-intensive/tree/main/02-week/kbot/cmd"
+
+func main() {
+	cmd.Execute()
+}
